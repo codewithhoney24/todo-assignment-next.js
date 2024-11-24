9ddcc0cdd8ae9484fe68a41552b85e085e0c8375
NOUSHEEN ATIF <161060487+codewithhoney24@users.noreply.github.com>
2024-11-24 05:54:41 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,28 +22,37 @@ export default function TodoApp() {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-r from-pink-400 via-purple-400 to-indigo-500 p-40">
-      <div className="max-w-xl mx-auto bg-white p-4 rounded-lg shadow-lg shadow-black ">
+    <div className="min-h-screen bg-gradient-to-r from-pink-400 via-purple-400 to-indigo-500 p-4 sm:p-8 md:p-12 lg:p-16">
+      <div className="max-w-xl mx-auto  mt-24 bg-white p-4 rounded-lg shadow-lg shadow-black">
         <h1 className="text-3xl font-bold text-center text-purple-600">
           🌈 Colorful ToDo App
         </h1>
-        <div className="mt-4 flex gap-2">
+        <div className="mt-4 flex gap-2 flex-wrap">
           <input
             type="text"
             value={task}
             onChange={(e) => setTask(e.target.value)}
             placeholder="Enter your task..."
-            className="flex-1 px-4 py-2 rounded-lg border border-pink-600 focus:outline-none focus:ring focus:ring-purple-300"
+            className="flex-1 px-4 py-2 rounded-lg border border-pink-600 focus:outline-none focus:ring focus:ring-purple-300 sm:w-full md:w-3/4"
           />
           <button
             onClick={addTask}
-            className= "font-bold px-4 py-2 bg-purple-500 text-white rounded-l   hover:bg-pink-400 "
+            className="font-bold px-4 py-2 bg-purple-500 text-white rounded-l hover:bg-pink-400 sm:w-full md:w-auto"
           >
             Add Task
           </button>
         </div>
         <TaskList  tasks={tasks} deleteTask={deleteTask}/>
       </div>
+
+      
+      <div className="container mt-10 mx-auto px-4 sm:px-8">
+  <h1 className="text-center text-fuchsia-950 text-lg sm:text-xl font-bold bg-gradient-to-r from-pink-400 via-purple-400 to-indigo-500 max-w-xl mx-auto bg-white p-4 rounded-lg shadow-lg shadow-black">
+    Assignment by Nousheen Atif
+  </h1>
+</div>
+      
     </div>
+    
   );
 }
